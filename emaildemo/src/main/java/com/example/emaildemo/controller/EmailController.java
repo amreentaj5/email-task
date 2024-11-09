1cd299f02021d34af193c8943bd240f911ea91ec
Amreen <amreentaj7848@gmail.com>
2024-11-09 20:38:19 +0530

    added task1

--- emaildemo/src/main/java/com/example/emaildemo/controller/EmailController.java
+++ emaildemo/src/main/java/com/example/emaildemo/controller/EmailController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EmailController {
-
+//Auto wired
     @Autowired
     private EmailService emailService;
 
